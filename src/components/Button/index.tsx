bc0ef4f6c746cd16428be2f961bef5f131e11031
Fellipe Barros <fellipe.barros@ztech.net>
2022-04-14 10:54:41 -0300

    add react base files

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button style={{ border: "none", padding: 0, margin: "10px" }}>
+      Heelloooooou
+    </button>
+  );
+};
+
+export default Button;

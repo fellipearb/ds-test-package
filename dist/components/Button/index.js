63c887b1220cc5d740c949073eae82acdf6b4cd5
Fellipe Barros <fellipe.barros@ztech.net>
2022-04-14 11:25:47 -0300

    add dist

---
+++ dist/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return /*#__PURE__*/React.createElement("button", {
+    style: {
+      border: "none",
+      padding: 0,
+      margin: "10px"
+    }
+  }, "Heelloooooou");
+};
+
+export default Button;

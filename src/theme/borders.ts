e7fc349fbd71e830a3b050ad1a6d09a85e63d2e3
Fellipe Barros <fellipe.barros@ztech.net>
2022-04-19 10:12:31 -0300

    add theme files

---
+++ src/theme/borders.ts
@@ -0,0 +1,16 @@
+export const borders = {
+  borderRadius: {
+    round: "100px",
+    default: "{space.scale.8}",
+    small: "{space.scale.4}",
+    none: "{space.scale.0}",
+  },
+  borderWidth: {
+    border: "1px",
+    "Token 1": {
+      style: "solid",
+      width: "{border.borderWidth.border}",
+      color: "{colors.neutral.borderHover}",
+    },
+  },
+};
---
+++ src/theme/index.ts
@@ -0,0 +1,6 @@
+export { theme } from "./theme";
+export { colors } from "./colors";
+export { typography } from "./typography";
+export { shadows } from "./shadows";
+export { borders } from "./borders";
+export { spacing } from "./spacing";
